93642791a68c70c24e0f3f17862fbe8b0fc74a22
vladyour <vlad.your8@gmail.com>
2023-03-16 21:07:13 +0100

    YKIT-5 Create button component * Add button styles * Add demo page

---
+++ src/app/button-demo/button-demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-demo',
+  templateUrl: './button-demo.component.html',
+  styleUrls: ['./button-demo.component.scss']
+})
+export class ButtonDemoComponent {
+
+}
